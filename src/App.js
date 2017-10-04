c84c86e5ea7700cd864119ac795a273a23181dee
Jesse R Weigel <jesse@jesseweigel.com>
2017-10-04 14:11:28 -0400

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { createStore, applyMiddleware, compose } from 'redux'
 import reducer from './reducers'
@@ -8,10 +7,9 @@ import thunk from 'redux-thunk'
 import './App.css'
 
 // Import Views
-// import Category from './views/Category'
-// import CreatePost from './views/CreatePost'
-// import Home from './views/Home'
-// import PostDetail from './views/PostDetail'
+import Category from './views/Category'
+import Home from './views/Home'
+import PostDetail from './views/PostDetail'
 
 const logger = store => next => action => {
   console.group(action.type)
@@ -32,17 +30,13 @@ const store = createStore(
 class App extends Component {
   render () {
     return (
-      <Provider>
+      <Provider store={store}>
         <BrowserRouter>
-          <div className='App'>
-            <header className='App-header'>
-              <img src={logo} className='App-logo' alt='logo' />
-              <h1 className='App-title'>Welcome to React</h1>
-            </header>
-            <p className='App-intro'>
-              To get started, edit <code>src/App.js</code> and save to reload.
-            </p>
-          </div>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/:category' component={Category} />
+            <Route path='/:category/:post_id' component={PostDetail} />
+          </Switch>
         </BrowserRouter>
 
       </Provider>
