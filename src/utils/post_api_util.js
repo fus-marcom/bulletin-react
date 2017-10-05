fe61045d759a0346219a853ef2749f0bbd936ff1
Harshit Pant <pantharshit00@gmail.com>
2017-10-06 02:53:13 +0530

    Redux removed in favour of graphQL

--- src/utils/post_api_util.js
+++
@@ -1,4 +0,0 @@
-//Get posts from WordPress
-export const fetchPosts = () =>
-  fetch('https://jesseweigel.com/wp-json/wp/v2/posts')
-  .then(data => data.json())
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,35 +1,11 @@
 import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { fetchPosts } from '../actions'
 // import { Link } from 'react-router-dom'
 // import '../styles/app.css'
 
 class Home extends Component {
-  componentWillMount () {
-    this.props.fetchData()
-  }
-
   render () {
-    return (
-      <div>
-        {this.props.posts &&
-          Object.values(this.props.posts)
-          .map(post =>
-            <h1 key={post.id}>{post.title.rendered}</h1>
-          )
-        }
-      </div>
-    )
+    return <div>PLACEHOLDER FOR YET</div>
   }
 }
 
-const mapStateToProps = state => ({
-  posts: state.receivePosts
-})
-
-const mapDispatchToProps = dispatch => ({
-  dispatch,
-  fetchData: () => dispatch(fetchPosts())
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(Home)
+export default Home
