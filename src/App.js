e76c17f1cfe404710df404a7accea7db7960b32a
SebastianCApostolescu <sebastianc.apostolescu@gmail.com>
2017-10-12 16:03:33 +0200

    refactorized App.js, removed App.css

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
-import React, { Component } from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import React from 'react'
 import { ApolloProvider } from 'react-apollo'
-import './styles/App.css'
 import client from './graphql/apolloClient'
+import Routes from './Routes'
 
-// Import Views
-import Category from './views/Category'
-import Home from './views/Home'
-import PostDetail from './views/PostDetail'
-
-class App extends Component {
-  render () {
-    return (
-      <ApolloProvider client={client}>
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/category/:slug" component={Category} />
-            <Route path="/post/:post_id" component={PostDetail} />
-          </Switch>
-        </BrowserRouter>
-      </ApolloProvider>
-    )
-  }
+const App = () => {
+  return (
+    <ApolloProvider client={client}>
+      <Routes />
+    </ApolloProvider>
+  )
 }
 
 export default App
---
+++ src/Routes.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+// Import Views
+import Category from './views/Category'
+import Home from './views/Home'
+import PostDetail from './views/PostDetail'
+
+const Routes = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/category/:slug" component={Category} />
+        <Route path="/post/:post_id" component={PostDetail} />
+      </Switch>
+    </BrowserRouter>
+  )
+}
+
+export default Routes
