1ae32915c3168e274bc3435e19069a26e4c77e4c
Harshit <pantharshit00@gmail.com>
2017-10-16 17:15:16 +0530

    Implemented on category page

--- src/views/Category.js
+++ src/views/Category.js
@@ -3,9 +3,8 @@ import { graphql } from 'react-apollo'
 import { getPostsByCat } from '../graphql/queries/posts'
 import Loader from '../components/Loader'
 import Layout from '../components/Layout/index'
-import PostPreview from '../components/PostPreview'
 import { Helmet } from 'react-helmet'
-import Grid from 'material-ui/Grid'
+import GridRenderer from '../components/GridTypes/GridRenderer'
 // import { Link } from 'react-router-dom'
 // import '../styles/app.css'
 
@@ -13,38 +12,14 @@ const Category = ({ data }) => {
   const isLoading = data.loading
   return (
     <Layout>
-      {isLoading && <Loader />}
-      {!isLoading && <RenderCategories data={data} />}
-    </Layout>
-  )
-}
-const RenderCategories = ({ data }) => {
-  const posts = data.posts
-  return (
-    <div>
       <Helmet>
         <title>
           Posts By Categories | Bulletin - Franciscan University of Steubenville
         </title>
       </Helmet>
-      <Grid container spacing={24}>
-        {posts &&
-          posts.edges.map(post => (
-            <Grid key={post.node.id} item xs={12} sm={6} md={4} lg={3}>
-              <PostPreview
-                style={{ maxWidth: '500px', margin: '0 auto' }}
-                key={post.node.id}
-                id={post.node.id}
-                date={post.node.date}
-                imageURL={
-                  post.node.featuredImage && post.node.featuredImage.sourceUrl
-                }
-                title={post.node.title}
-              />
-            </Grid>
-          ))}
-      </Grid>
-    </div>
+      {isLoading && <Loader />}
+      {!isLoading && <GridRenderer posts={data.posts} />}
+    </Layout>
   )
 }
 
