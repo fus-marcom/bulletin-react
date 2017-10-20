81c338e61cac90b35c2695d4e6c92d080fe1cfc2
Rud156 <rudrangshu_1997@yahoo.com>
2017-10-20 09:55:03 +0530

    Updated with Continue Reading

--- src/components/GridTypes/GridView.js
+++ src/components/GridTypes/GridView.js
@@ -1,24 +1,47 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Grid from 'material-ui/Grid'
 import PostPreview from '../PostPreview'
 
-const GridView = ({ posts }) => (
-  <Grid container>
-    {posts.edges.map(post => (
-      <Grid key={post.node.id} item xs={6} sm={4} md={6} lg={6}>
-        <PostPreview
-          id={post.node.id}
-          date={post.node.date}
-          imageURL={
-            post.node.featuredImage && post.node.featuredImage.sourceUrl
-          }
-          title={post.node.title}
-          category={post.node.categories.edges[0].node.name}
-          content={post.node.content}
-        />
+class GridView extends Component {
+  constructor (props) {
+    super(props)
+
+    this.truncateChecker = this.truncateChecker.bind(this)
+  }
+
+  truncateChecker (node) {
+    let value = node.content
+    if (value.length > 300) {
+      return (
+        value.substring(0, 300) +
+        ` ... <a href="/post/${node.id}" class="PostPreview-titleColor-248" style="text-decoration: none">Continue Reading</a>`
+      )
+    } else {
+      return value
+    }
+  }
+
+  render () {
+    let posts = this.props.posts
+    return (
+      <Grid container>
+        {posts.edges.map(post => (
+          <Grid key={post.node.id} item xs={6} sm={4} md={6} lg={6}>
+            <PostPreview
+              id={post.node.id}
+              date={post.node.date}
+              imageURL={
+                post.node.featuredImage && post.node.featuredImage.sourceUrl
+              }
+              title={post.node.title}
+              category={post.node.categories.edges[0].node.name}
+              content={this.truncateChecker(post.node)}
+            />
+          </Grid>
+        ))}
       </Grid>
-    ))}
-  </Grid>
-)
+    )
+  }
+}
 
 export default GridView
