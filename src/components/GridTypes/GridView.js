63d3c7c91dd86a13e176c01935de421215fedc44
Harshit <pantharshit00@gmail.com>
2017-10-23 00:14:45 +0530

    Refactored Grid to stateless component

--- src/components/GridTypes/GridView.js
+++ src/components/GridTypes/GridView.js
@@ -3,36 +3,28 @@ import PostPreview from '../PostPreview'
 import Masonry from 'react-masonry-component'
 import '../../styles/grid.css'
 
-class GridView extends React.Component {
-  state = {
-    test: ''
-  }
-  render () {
-    const { posts } = this.props
-    return <div className="row">{this.renderPosts(posts)}</div>
-  }
-  renderPosts (posts) {
-    return (
-      <div className="container-fluid">
-        <Masonry>
-          {posts.edges.map((post, i) => (
-            <div className="col s12 m12 l6 xl4" key={i}>
-              <PostPreview
-                date={post.node.date}
-                imageURL={
-                  post.node.featuredImage && post.node.featuredImage.sourceUrl
-                }
-                title={post.node.title}
-                category={post.node.categories.edges[0].node.name}
-                content={post.node.content}
-                trim={true}
-              />
-            </div>
-          ))}
-        </Masonry>
-      </div>
-    )
-  }
-}
+const GridView = ({ posts }) => (
+  <div className="row">
+    <div className="container-fluid">
+      <Masonry>
+        {posts.edges.map(post => (
+          <div className="col s12 m12 l6 xl4" key={post.node.id}>
+            <PostPreview
+              date={post.node.date}
+              imageURL={
+                post.node.featuredImage && post.node.featuredImage.sourceUrl
+              }
+              id={post.node.id}
+              title={post.node.title}
+              category={post.node.categories.edges[0].node.name}
+              content={post.node.content}
+              trim={true}
+            />
+          </div>
+        ))}
+      </Masonry>
+    </div>
+  </div>
+)
 
 export default GridView
