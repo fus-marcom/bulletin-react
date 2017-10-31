ff0a177db7add86aa16c93dae44f705f6e0d10a9
SebastianCApostolescu <sebastianc.apostolescu@gmail.com>
2017-10-31 14:15:49 +0100

    fixed attachment code

--- src/components/Attachments.js
+++ src/components/Attachments.js
@@ -10,14 +10,16 @@ const Attachments = ({ attachments }) => {
         Attachments:
       </Typography>
 
+      {/* attachments.map((attachment, i) => (
+            <Typography gutterBottom key={i}>
+              <Button dense color="primary">
+                <AttachmentIcon /> {attachment}
+              </Button>
+            </Typography>
+          )) */}
       <Typography gutterBottom>
         <Button dense color="primary">
           <AttachmentIcon /> Attachment1
-          {/* attachments.map((attachment, i) => (
-            <div key={i}>
-              <AttachmentIcon /> {attachment}
-            </div>
-          )) */}
         </Button>
       </Typography>
     </div>
